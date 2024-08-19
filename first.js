9c5541858f76531483ea9ccb91461eb2388e73f6
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-08-19 10:58:44 +0600

    commited

---
+++ first.js
@@ -0,0 +1,21 @@
+console.log('first file');
+
+
+const allLi = document.getElementsByTagName('li');
+console.log(allLi);
+
+
+const allTitles = document.getElementsByClassName('section-title');
+console.log(allTitles);
+const secondSection = document.getElementById('second-section');
+console.log(secondSection);
+secondSection.style.color = 'white';
+secondSection.style.backgroundColor = 'tomato';
+
+
+
+
+
+
+
+
---
+++ second.js
@@ -0,0 +1,21 @@
+console.log('second file');
+
+const secondList = document.getElementById('second-list');
+console.log(secondList);
+
+const li = document.createElement('li');
+li.innerText = 'my dynamic list';
+secondList.appendChild(li);
+
+
+
+
+
+
+
+
+
+
+
+
+
