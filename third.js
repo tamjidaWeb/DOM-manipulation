54aa38c748b57fb6892cfa9ffc64d8f99f63f5c1
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-21 21:16:09 +0600

    commited

--- third.js
+++ third.js
@@ -1,6 +1,14 @@
 const main = document.getElementById('main-container');
 const section = document.createElement('section');
 section.innerHTML = `
-
+    <h1> my dynamic sectionnn </h1>
+    <p> Extra text added inside the paragraph</p>
+    <ul>
+        <li> first item</li>
+         <li> second item</li>
+          <li> third item</li>
+           <li> fourth item</li>
+           </ul>
+`
 
 main.appendChild(section);
