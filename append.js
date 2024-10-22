3889e4f81ba26a633be76f948e14ea328a994fe8
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-22 16:54:31 +0600

    commited

--- append.js
+++ append.js
@@ -1,22 +1,16 @@
-//where to add
-const placesList = document.getElementById('places-list');
-//what to be added
+const placesList = document.getElementById('places-list')
 const li = document.createElement('li');
-li.innerText = 'pahartoli bon';
-//3 add the append child
+li.innerText = 'paharToli bon';
 placesList.appendChild(li);
 
-
-//where to add 
-
-const mainContainer = document.getElementById('main-content')
-const section =document.createElement('section');
+const mainContainer = document.getElementById('main-content');
+const section = document.createElement('section');
 const h1 = document.createElement('h1');
-h1.innerText = 'my food list';
+h1.innerText= 'My Food List';
 section.appendChild(h1);
 const ul = document.createElement('ul');
-const li1= document.createElement('li');
-li1.innerText= 'biryani';
+const li1 = document.createElement('li');
+li1.innerText = 'biryani';
 ul.appendChild(li1);
 const li2 = document.createElement('li');
 li2.innerText = 'borhani';
@@ -24,20 +18,7 @@ ul.appendChild(li2);
 const li3 = document.createElement('li');
 li3.innerText = 'salad';
 ul.appendChild(li3);
-section.appendChild(ul);
-mainContainer.appendChild(section);
+section.appendChild(ul)
 
-//set html directly
 
-const sectionDress = document.createElement('section');
-sectionDress.innerHTML= `
-<h1>My dress section</h1>
-<ul>
-    <li>T-shirt</li>
-    <li>Lungi</li>
-    <li>sando genji</li>
-</ul>
-
-`
-mainContainer.appendChild(sectionDress)
-
+mainContainer.appendChild(section)
