7a060b013333a349d56d44597d2291e00bde6120
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-22 18:09:16 +0600

    commited

--- first.js
+++ first.js
@@ -1,1 +1,10 @@
-console.log('first file')
+console.log('first file');
+const allLi = document.getElementById('li');
+
+const allTitles = document.getElementsByClassName('section-title')
+console.log(allTitles);
+
+const secondSection = document.getElementById('second-section');
+console.log(secondSection)
+secondSection.style.color = 'blue';
+secondSection.style.backgroundColor = 'violet';
--- second.js
+++ second.js
@@ -1,1 +1,6 @@
-console.log('second file')
+// console.log('second file')
+
+const secondId = document.getElementById('second-list');
+const li = document.createElement('li');
+li.innerText = 'purple is my fav color';
+secondId.appendChild(li);
