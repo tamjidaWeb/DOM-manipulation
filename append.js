02ffb645e8066980e721db079de7c5e996b831fd
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-22 15:53:44 +0600

    commited

--- append.js
+++ append.js
@@ -7,7 +7,25 @@ li.innerText = 'pahartoli bon';
 placesList.appendChild(li);
 
 
+//where to add 
 
+const mainContainer = document.getElementById('main-content')
+const section =document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'my food list';
+section.appendChild(h1);
+const ul = document.createElement('ul');
+const li1= document.createElement('li1');
+li1.innerText= 'biryani';
+const li2 = document.createElement('li2');
+li2.innerText = 'borhani';
+const li3 = document.createElement('li3');
+li2.innerText = 'salad';
+
+
+
+
+// mainContainer.appendChild(section);
 
 
 
