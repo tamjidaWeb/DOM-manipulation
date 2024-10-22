d43c1099637606fa45e51928ec719e8796877c5b
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-10-22 15:00:54 +0600

    commited

--- append.js
+++ append.js
@@ -1,69 +1,10 @@
 //where to add
 const placesList = document.getElementById('places-list');
-//where to be added
-const li = document.createElement('li');
-li.innerText ='pahartoli bon';
-
-
-// add the child
-placesList.appendChild(li);
-
-//where to add
-const mainContainer = document.getElementById('main-container');
 //what to be added
-const section = document.createElement('section');
-const h1 = document.createElement('h1');
-h1.innerText = 'my food list';
-section.appendChild(h1);
-const ul = document.createElement('ul');
-const li1 = document.createElement('li');
-li1.innerText='biryani';
-ul.appendChild(li1);
-const li2 = document.createElement('li');
-li2.innerText = 'borhani';
-ul.appendChild(li2);
-const li3 = document.createElement('li');
-li3.innerText = 'chatni';
-ul.appendChild(li3);
-
-section.appendChild(ul);
-mainContainer.appendChild(section);
-
-
-
-//set innerHTML directly
-
-const sectionDress = document.createElement('section');
-sectionDress.innerHTML = `
-<h1> My dress Section </h1>
-
-<ul>
-    <li> T-shirt </li>
-    <li> Three - piece </li>
-    <li> Pajama </li>
-    <li> Ganji </li>
-    </ul>
-    `
-
-mainContainer.appendChild(sectionDress);
-
-
-
-
-
-
-
-
-
-
-
-
-
-mainContainer.appendChild(section);
-
-
-
-
+const li = document.createElement('li');
+li.innerText = 'pahartoli bon';
+//3 add the append child
+placesList.appendChild(li);
 
 
 
